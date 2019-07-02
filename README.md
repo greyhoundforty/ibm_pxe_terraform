a7883a5d6806ca5be34f89059c77a95133305746
greyhoundforty <ryantiffany@fastmail.com>
2019-07-02 10:50:45 -0500

    Moved dnsmasq config generation away from templates

--- README.md
+++ README.md
@@ -16,4 +16,10 @@ Example of how to deploy a PXE VSI. The code in this example will:
 
 ## Todos
  - [x] Switch from DHCP and TFTP to dnsmasq as it has built in TFTP support and is more robust (Ansible)
- - [ ] Look at recursive copy [issue](https://github.com/greyhoundforty/ibm_pxe_terraform/issues/3)
+ - [x] Look at recursive copy [issue](https://github.com/greyhoundforty/ibm_pxe_terraform/issues/3)
+
+
+## Configure environment
+ - Install Terraform and the IBM Terraform provider using this [guide](https://cloud.ibm.com/docs/terraform?topic=terraform-getting-started#install).
+ - Install [Ansible](https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html) 
+ - Create a `credentials.tfvars` file or set [environmental variables](https://ibm-cloud.github.io/tf-ibm-docs/v0.17.1/) for IBM authentication.
--- main.tf
+++ main.tf
@@ -14,16 +14,20 @@ resource "ibm_network_vlan" "pxe_vlan" {
   datacenter      = "${var.datacenter["us-east2"]}"
   type            = "PRIVATE"
   router_hostname = "bcr01a.${var.datacenter["us-east2"]}"
+
+  timeouts {
+    delete = "30m"
+  }
 }
 
 # Create a subnet for DHCP server 
 resource "ibm_subnet" "dhcp_subnet" {
-  type = "Portable"
-  private = true
+  type       = "Portable"
+  private    = true
   ip_version = 4
-  capacity = 8
-  vlan_id = "${ibm_network_vlan.pxe_vlan.id}"
-  notes = "dhcp testing subnet"
+  capacity   = 8
+  vlan_id    = "${ibm_network_vlan.pxe_vlan.id}"
+  notes      = "dhcp testing subnet"
 }
 
 # Create our VSI PXE instance 
@@ -54,6 +58,36 @@ EOF
   filename = "${path.cwd}/Hosts/inventory.env"
 }
 
+# Create our dnsmasq.conf file for PXE server
+resource "local_file" "dnsmasq_config" {
+  depends_on = ["local_file.ansible_hosts"]
+
+  content = <<EOF
+---
+  - hosts: vm_instances
+    become: true
+    tasks:
+    - name: "Update dnsmasq conf"
+      blockinfile:
+          backup: yes
+          path: /etc/dnsmasq.conf
+          block: |
+              interface=eth0,lo
+              bind-interfaces
+              listen-address=${ibm_compute_vm_instance.pxe_server.ipv4_address_private},127.0.0.1
+              domain=cdetesting.local
+              dhcp-range=${cidrhost(ibm_subnet.dhcp_subnet.subnet_cidr, 2)},${cidrhost(ibm_subnet.dhcp_subnet.subnet_cidr, 6)},${cidrnetmask(ibm_subnet.dhcp_subnet.subnet_cidr)},1h
+              dhcp-option=3,${cidrhost(ibm_subnet.dhcp_subnet.subnet_cidr, 1)}
+              dhcp-option=6,8.8.8.8,4.2.2.2
+              server=8.8.8.8
+              dhcp-boot=/pxelinux.0,pxeserver,${ibm_compute_vm_instance.pxe_server.ipv4_address_private}
+              enable-tftp
+              tftp-root=/var/lib/tftpboot/
+EOF
+
+ filename = "${path.cwd}/Playbooks/dnsmasq.yml"
+}
+
 resource "local_file" "curl_body" {
   depends_on = ["local_file.ansible_hosts"]
 
@@ -64,7 +98,7 @@ resource "local_file" "curl_body" {
       "subjectId": 1061,
       "title": "Set DHCP helper IP for PXE boot"
     },
-    "Please set the DHCP helper IP on VLAN ${ibm_network_vlan.pxe_vlan.id} in the ${var.datacenter["us-east2"]} DC to ${ibm_compute_vm_instance.pxe_server.ipv4_address_private}."
+    "Please set the DHCP helper IP on VLAN ${ibm_network_vlan.pxe_vlan.id} in the ${var.datacenter["us-east2"]} DC to ${ibm_compute_vm_instance.pxe_server.ipv4_address_private}. If the VLAN already has a DHCP helper IP please remove it and replace it with the one listed in this ticket."
   ]
 }
 EOF
@@ -72,44 +106,49 @@ EOF
   filename = "${path.cwd}/ticket.json"
 }
 
-// resource "null_resource" "create_ticket" {
-//   depends_on = ["local_file.curl_body"]
+resource "null_resource" "create_ticket" {
+  depends_on = ["local_file.curl_body"]
 
-//   provisioner "local-exec" {
-//     command = "curl -u ${var.ibm_sl_username}:${var.ibm_sl_api_key} -X POST -H 'Accept: */*' -H 'Accept-Encoding: gzip, deflate, compress' -d @${path.cwd}/ticket.json 'https://api.softlayer.com/rest/v3.1/SoftLayer_Ticket/createStandardTicket.json'"
-//   }
-// }
-
-# Run ansible playbook to install and configure TFTP/DHCP/Webroot
-// resource "null_resource" "run_playbook" {
-//   // depends_on = ["null_resource.create_ticket"]
-//     depends_on = ["local_file.curl_body"]
-//   provisioner "local-exec" {
-//     command = "ansible-playbook -i Hosts/inventory.env Playbooks/server-config.yml"
-//   }
-// }
-
-output "subnet_cidr" {
-  value = "${ibm_subnet.dhcp_subnet.subnet_cidr}"
-}
-
-data "template_file" "init" {
-  template = "${file("${path.cwd}/Templates/dnsmasq.tpl")}"
-
-  vars = {
-    first_usable_ip = "${cidrhost(ibm_subnet.dhcp_subnet.subnet_cidr, 2)}"
-    last_usable_ip = "${cidrhost(ibm_subnet.dhcp_subnet.subnet_cidr, 6)}"
-    subnet_netmask = "${cidrnetmask(ibm_subnet.dhcp_subnet.subnet_cidr)}"
-    subnet_gw = "${cidrhost(ibm_subnet.dhcp_subnet.subnet_cidr, 1)}"
-    pxe_ip = "${ibm_compute_vm_instance.pxe_server.ipv4_address_private}"
+  provisioner "local-exec" {
+    command = "curl -u ${var.ibm_sl_username}:${var.ibm_sl_api_key} -X POST -H 'Accept: */*' -H 'Accept-Encoding: gzip, deflate, compress' -d @${path.cwd}/ticket.json 'https://api.softlayer.com/rest/v3.1/SoftLayer_Ticket/createStandardTicket.json'"
   }
 }
 
-resource "local_file" "dnsmasq_playbook" {
-content = <<EOF
-  ${data.template_file.init.rendered}
-
-EOF
-
-  filename = "${path.cwd}/Playbooks/dnsmasq.yml"
+# Run ansible playbook to install and configure TFTP/DHCP/Webroot
+resource "null_resource" "run_playbook" {
+  // depends_on = ["null_resource.create_ticket"]
+    depends_on = ["local_file.curl_body"]
+  provisioner "local-exec" {
+    command = "ansible-playbook -i Hosts/inventory.env Playbooks/server-config.yml"
+  }
 }
+
+resource "ibm_compute_bare_metal" "no_os" {
+  # Mandatory fields
+  package_key_name = "DUAL_E52600_V4_12_DRIVES"
+  process_key_name = "INTEL_INTEL_XEON_E52620_V4_2_10"
+  memory           = 64
+  os_key_name      = "OS_NO_OPERATING_SYSTEM"
+  hostname         = "no-os"
+  domain           = "${var.domain}"
+  datacenter       = "${var.datacenter["us-east2"]}"
+  network_speed    = 10000
+  public_bandwidth = 500
+  disk_key_names   = ["HARD_DRIVE_800GB_SSD", "HARD_DRIVE_800GB_SSD", "HARD_DRIVE_800GB_SSD"]
+  hourly_billing   = false
+
+  # Optional fields
+  private_network_only = false
+  unbonded_network     = true
+
+  public_vlan_id  = "${ibm_compute_vm_instance.pxe_server.public_vlan_id}"
+  private_vlan_id = "${ibm_network_vlan.pxe_vlan.id}"
+
+  tags = [
+    "pxe-test",
+    "ryantiffany",
+  ]
+
+  redundant_power_supply = true
+}
+
